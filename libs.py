e65efa4fb108c7a0dbce835ebbb3b1e95b38608e
mducducd <37109868+mducducd@users.noreply.github.com>
2022-02-28 10:51:02 +0700

    Update libs.py

--- libs.py
+++ libs.py
@@ -1,3 +1,4 @@
 import cv2
 import numpy as np
-from skimage.metrics import structural_similarity as ssim
+# from skimage.metrics import structural_similarity as ssim
+from modified_ssim import _ssim as ssim
